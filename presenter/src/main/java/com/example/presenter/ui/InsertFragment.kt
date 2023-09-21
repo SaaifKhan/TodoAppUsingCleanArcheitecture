082eff2d9f0b0b5a1ebd317e19a3ea0e5395297f
Saif Khan <saif_khan09@Hotmail.com>
2023-09-21 19:25:16 +0500

    Toolbar setup

--- presenter/src/main/java/com/example/presenter/ui/InsertFragment.kt
+++ presenter/src/main/java/com/example/presenter/ui/InsertFragment.kt
@@ -3,9 +3,16 @@ package com.example.presenter.ui
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import com.example.presenter.R
 import com.example.presenter.databinding.FragmentInsertBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,6 +33,25 @@ class InsertFragment : Fragment(R.layout.fragment_insert) {
         _binding = FragmentInsertBinding.bind(view)
         binding?.viewmodel = viewModel
         binding?.lifecycleOwner = viewLifecycleOwner
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider{
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.add_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.menu_add){
+                    Toast.makeText(requireContext(),"Add",Toast.LENGTH_LONG).show()
+                }else if (menuItem.itemId == androidx.appcompat.R.id.home){
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+
+
+                }
+                return true
+            }
+
+        },viewLifecycleOwner,Lifecycle.State.RESUMED)
 
 
     }
